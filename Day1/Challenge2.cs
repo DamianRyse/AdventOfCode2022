a81c9351338b1975919c760c694a5ca097e46b8b
Damian Ryse <damianryse@gmail.com>
2022-12-07 11:57:06 +0100

    Day1/Challenge2 put into separate file.

---
+++ Day1/Challenge2.cs
@@ -0,0 +1,24 @@
+﻿using DamianRyse.AdventOfCode2022.Day1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DamianRyse.AdventOfCode2022.Day1
+{
+    internal class Challenge2
+    {
+        private List<Challenge1.Elf> Elfes;
+        public Challenge2(IEnumerable<Challenge1.Elf> elfes)
+        {
+            Elfes = elfes.ToList();
+        }
+        public void Start()
+        {
+            Challenge1.Elf[] top3 = Elfes.OrderByDescending(elf => elf.SumCalories).Take(3).ToArray();
+            Console.WriteLine($"Day1/Challenge 2 => Total calories of the top 3: {top3.Sum(elf => elf.SumCalories)}");
+        }
+
+    }
+}
